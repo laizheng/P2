76241c9c2219064b44fb75345ff94f354b60bb24
gdlaizheng@gmail.com <lz871014>
2017-01-25 22:55:21 +0800

    remove downloading command in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-wget https://d17h27t6h515a5.cloudfront.net/topher/2016/November/581faac4_traffic-signs-data/traffic-signs-data.zip
-unzip traffic-signs-data.zip
+cp ../train.p ./
+cp ../test.p ./
 python dump_pickle_to_files.py
 python generate_jittered_image.py
 python main.py
