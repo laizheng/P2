8777144498f63d21634ab4cc7fe483dda727de2f
gdlaizheng@gmail.com <lz871014>
2017-01-26 18:26:27 +0800

    use keras

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.cross_validation import train_test_split
 #from classifer import CNN
 #from CNN_keras import CNN
-#from CNN_keras_debug import CNN
-from CNN_from_nparray import CNN
+from CNN_keras_debug import CNN
+#from CNN_from_nparray import CNN
 
 def main():
     cnn = CNN(use_gray = True, use_jitter = False)
-    cnn.train(epochs=1000,batch_size=32)
+    cnn.train(epochs=200,batch_size=256)
     pass
 if __name__ == "__main__":
     main()
