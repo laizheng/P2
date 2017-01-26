22c25722b70487fa067a5e0853661cbf848e21af
gdlaizheng@gmail.com <lz871014>
2017-01-26 14:19:22 +0800

    use model built from tf flow ground up

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.cross_validation import train_test_split
 #from classifer import CNN
 #from CNN_keras import CNN
-from CNN_keras_debug import CNN
+#from CNN_keras_debug import CNN
+from CNN_from_nparray import CNN
 
 def main():
     cnn = CNN(use_gray = True, use_jitter = False)
